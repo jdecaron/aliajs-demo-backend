35856ebf96fda77b80fcbee3d1f6723e31ae21b2
Jean Denis Caron <j@rotat.io>
2025-05-22 12:30:35 -0400

    Fix update client2

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ app.get('/demo-95d8e9f5', async (c) => {
 })
 
 app.get('/demo-update-client2', async (c) => {
-  clients.client2 = new Redis(`redis://${c.host}:6379`)
+  clients.client2 = new Redis(`redis://${c.req.query().host}:6379`)
 })
 
 serve({
