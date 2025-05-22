a7e09a39a07c45042cf99582e4461dadcfcca695
Jean Denis Caron <j@rotat.io>
2025-05-22 11:09:17 -0400

    Add redis shoestring balancer.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import Redis from 'ioredis'
 
+const clients = {}
+function redis() {
+  return clients.client2 || clients.client1
+}
+
 try {
   // @ts-ignore
-  var client = new Redis(process.env.REDIS_URL)
+  clients.client1 = new Redis(process.env.REDIS_URL)
 } catch (error) {}
 
 const app = new Hono()
@@ -14,8 +19,12 @@ const app = new Hono()
 app.use('/*', cors())
 
 app.get('/demo-95d8e9f5', async (c) => {
-    await client.set('foo', 'bar')
+    await redis().rpush(c.req.query().hash, c.req.query().counter)
     return c.json({})
+})
+
+app.get('/demo-update-client2', async (c) => {
+  clients.client2 = new Redis(`redis://${c.host}:6379`)
 })
 
 serve({
